44b30d415c93dd5bcf131afc76ff789d2210c575
zhanglu <comely_zl@163>
2020-05-20 21:30:13 +0800

    提交客户端服务端代码

--- netty-study/src/main/java/com/beinglee/nettystudy/client/NettyClient.java
+++ netty-study/src/main/java/com/beinglee/nettystudy/client/NettyClient.java
@@ -1,8 +1,53 @@
 package com.beinglee.nettystudy.client;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author zhanglu
  * @date 2020/5/19 21:04
  */
 public class NettyClient {
+
+    private static final int MAX_RETRY = 5;
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8000;
+
+
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                .group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        System.out.println("客户端启动中...");
+                    }
+                });
+        connect(bootstrap, HOST, PORT, MAX_RETRY);
+    }
+
+    private static void connect(final Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.out.println("连接失败!");
+            } else {
+                int order = (MAX_RETRY - retry) + 1;
+                int delay = 1 << order;
+                System.out.println(LocalDateTime.now() + "：连接失败，第" + order + "次重连...");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+            }
+        });
+
+    }
 }
--- netty-study/src/main/java/com/beinglee/nettystudy/server/NettyServer.java
+++ netty-study/src/main/java/com/beinglee/nettystudy/server/NettyServer.java
@@ -1,8 +1,56 @@
 package com.beinglee.nettystudy.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * @author zhanglu
  * @date 2020/5/19 21:04
  */
 public class NettyServer {
+
+    private static final int PORT = 8000;
+
+    public static void main(String[] args) {
+        // 监听端口，accept新连接的线程组
+        NioEventLoopGroup masterGroup = new NioEventLoopGroup();
+        // 用来处理每一条连接 进行数据读写的线程组
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap server = new ServerBootstrap();
+        // 指定线程模型
+        server.group(masterGroup, workerGroup)
+                // 指定IO模型
+                .channel(NioServerSocketChannel.class)
+                // 指定在服务端启动过程中的一些逻辑
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+                        System.out.println("服务端启动中...");
+                    }
+                })
+                // 指定连接读写处理逻辑
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel channel) {
+                        System.out.println("客户端连接成功...");
+                    }
+                });
+        bind(server, PORT);
+    }
+
+    private static void bind(final ServerBootstrap server, final int port) {
+        server.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功！奥利给！");
+            } else {
+                System.out.println("端口[" + port + "]绑定失败！");
+                bind(server, port + 1);
+            }
+        });
+    }
+
 }
